f948d4f024fcc2b5c402006505bc0cadf9c3d895
TimzOwen <ktimothy.com>
2022-02-15 12:05:31 +0300

    more beginner tests added

--- python_practice/beginner/basic/basicTest.py
+++ python_practice/beginner/basic/basicTest.py
@@ -33,3 +33,58 @@ if (n%2==0):
         print("Not Weird")
 else:
     print("Weird")
+
+
+
+# The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+
+# The first line contains the sum of the two numbers.
+# The second line contains the difference of the two numbers (first - second).
+# The third line contains the product of the two numbers
+
+a = int(raw_input())
+b = int(raw_input())
+
+print(a+b)
+print(a-b)
+print(a*b)
+
+
+# The provided code stub reads two integers,  and , from STDIN.
+# Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+# No rounding or formatting is necessary.
+
+from __future__ import division
+
+if __name__ == '__main__':
+    a = int(raw_input())
+    b = int(raw_input())
+
+print(a//b)
+print(a/b)
+
+
+# The provided code stub reads and integer, , from STDIN. For all non-negative integers , print .
+# Example
+# The list of non-negative integers that are less than  is . Print the square of each number on a separate line.
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    
+    for i in range(n):
+        print(i**2)
+    
+    
+# An extra day is added to the calendar almost every four years as February 29, and the day is called a leap day. It corrects the calendar
+#  for the fact that our planet takes approximately 365.25 days to orbit the sun. A leap year contains a leap day.
+# In the Gregorian calendar, three conditions are used to identify leap years:
+
+# The year can be evenly divided by 4, is a leap year, unless:
+# The year can be evenly divided by 100, it is NOT a leap year, unless:
+# The year is also evenly divisible by 400. Then it is a leap year.
+# This means that in the Gregorian calendar, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300 and 2500 are NOT leap years. Source
+
+# Task
+# Given a year, determine whether it is a leap year. If it is a leap year, return the Boolean True, otherwise return False.
+# Note that the code stub provided reads from STDIN and passes arguments to the is_leap function. It is only necessary to complete the is_leap function.
+
