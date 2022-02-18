95ec00214fc0d7bd70c5ba0210f6a998a8a79358
TimzOwen <ktimothy.com>
2022-02-18 11:30:23 +0300

    updated the swap

--- python_practice/beginner/basic/basicTest.py
+++ python_practice/beginner/basic/basicTest.py
@@ -349,5 +349,24 @@ if __name__ == '__main__':
     print(hash(t))
 
 #You are given a string and your task is to swap cases. In other words, convert all lowercase letters to uppercase letters and vice versa.
+def swap_case(s):
+    
+    return s.swapcase()
+if __name__ == '__main__':
+    s = raw_input()
+    result = swap_case(s)
+    print result
 
-
+#soln 2
+def swap_case(s):
+    result = ''
+    for letter in s:
+        if letter == letter.upper():
+            letter += letter.lower()
+        else:
+            letter += letter.lower()
+    return result
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
