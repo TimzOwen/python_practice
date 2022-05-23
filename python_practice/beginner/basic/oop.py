4874d148440dde4b044e252c73da67fe5c3c42ea
TimzOwen <timzowen@gmail.com>
2022-05-23 19:24:59 +0300

    oop test

---
+++ python_practice/beginner/basic/oop.py
@@ -0,0 +1,18 @@
+# function 
+def greeting_message(school,name):
+    print(f"from of {school} we welcome home Engineer {name}") 
+
+greeting_message("set","Timz")
+
+
+# Lists 
+
+mylist = ["one","two","three","Four"]
+print(mylist)
+
+
+# iterate lists 
+
+mydata = ["one","Two","three","Four","Five","six"]
+for data in mydata:
+    print(data)
--- python_practice/beginner/basic/test_runer.py
+++
@@ -1,4 +0,0 @@
-def greeting_message(school,name):
-    print(f"from {school} we welcome home {name}") 
-
-greeting_message("set","Timz")
