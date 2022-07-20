faad157c1a35db861ae2419f8962859c720baaaa
TimzOwen <timzowen@gmail.com>
2022-07-20 06:01:29 +0300

    rock , scissor and paper

--- python_practice/beginner/basic/basicTest.py
+++ python_practice/beginner/basic/basicTest.py
@@ -520,3 +520,59 @@ def checkPalidrone():
 list_a = [2,3,5,7,9,12,38,90]
 list_even = [x for x in list_a if x % 2 == 0]
 print(list_even)
+
+
+# Guess Game with while and if conditions;
+
+def rock_scissor_paper():
+    user_input = input("Enter a guess!: ")
+    while user_input != "quit":
+        if(user_input == "Rock"):
+            print("Rock won....")
+            break
+        elif (user_input == "Scissors"):
+            print("Scissors Won..")
+            break
+        elif(user_input == "Paper"):
+            print("Paper won....")
+            break
+        else:
+            user_input = input("Try another Guess. ")
+    print("Game Ended...")
+rock_scissor_paper()
+
+
+
+# Rock paper and scissor Game:
+
+import sys
+
+user_name1 = input("Hello, What your name? ")
+user_name2 = input("And you? ")
+user_1 = input(f"Hey {user_name1}, Do you want to choose rock, scissor or paper?...")
+user_2 = input(f"what about you {user_name2}, what do you choose?...")
+
+def compare_game(u1, u2):
+    if u1 == u2:
+        return " Its a tie for you two"
+    elif(u1 == "rock"):
+        if (u2 == "scissor"):
+            return "Rock wins !!"
+        else:
+            return "Paper wins !!"
+    elif (u1 == "scissor"):
+        if(u2 == "paper"):
+            return "Scissor wins !!"
+        else:
+            return "Rock wins !!"
+    elif (u1 == "paper"):
+        if(u2== "rock"):
+            return "Paper wins !!"
+        else:
+            return "Scissor wins !!"
+    else:
+        return "Try again, you did not select either Rock, Scissor or Paper"
+        sys.exit()
+
+print(compare_game(user_1,user_2))
+
--- python_practice/beginner/basic/test_runer.py
+++ python_practice/beginner/basic/test_runer.py
@@ -1,3 +1,32 @@
-a = [2,1,4,7,9,8,12,60,6,2,7,90]
-new_a = [x for x in a if x % 2 == 0 ]
-print(new_a)
+# Rock paper and scissor Game:
+
+import sys
+
+user_name1 = input("Hello, What your name? ")
+user_name2 = input("And you? ")
+user_1 = input(f"Hey {user_name1}, Do you want to choose rock, scissor or paper?...")
+user_2 = input(f"what about you {user_name2}, what do you choose?...")
+
+def compare_game(u1, u2):
+    if u1 == u2:
+        return " Its a tie for you two"
+    elif(u1 == "rock"):
+        if (u2 == "scissor"):
+            return "Rock wins !!"
+        else:
+            return "Paper wins !!"
+    elif (u1 == "scissor"):
+        if(u2 == "paper"):
+            return "Scissor wins !!"
+        else:
+            return "Rock wins !!"
+    elif (u1 == "paper"):
+        if(u2== "rock"):
+            return "Paper wins !!"
+        else:
+            return "Scissor wins !!"
+    else:
+        return "Try again, you did not select either Rock, Scissor or Paper"
+        sys.exit()
+
+print(compare_game(user_1,user_2))
