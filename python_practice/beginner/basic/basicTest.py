dc06abd9eba1bf23fcb986b13ee48a29e197b9a8
TimzOwen <timzowen@gmail.com>
2022-07-21 17:52:52 +0300

    guess game

--- python_practice/beginner/basic/basicTest.py
+++ python_practice/beginner/basic/basicTest.py
@@ -760,19 +760,34 @@ def fibonacci():
     return fib
 print(fibonacci())
 
-# Create a program that will play the “cows and bulls” game with the user.
-# The game works like this:Randomly generate a 4-digit number. Ask the user to 
-# guess a 4-digit number.For every digit that the user guessed correctly in the 
-# correct place, theyhave a “cow”. For every digit the user guessed correctly in 
-# the wrong place is a “bull.” Every time the user makes a guess, tell them how many 
-# “cows” and“bulls” they have. Once the user guesses the correct number, the game is over.
-# Keep track of the number of guesses the user makes throughout teh game and tell the user 
-# at the end.
+
+# computer game guess(user keeps guess number and the computer tries to guess)
+def guessing_game():
+    high_range = int(input("What is the number range?\n1 to ..."))
+    low_range = 1
+    midpoint = int(high_range / 2)
+    guess_count = 0
+
+    while True:
+        print(f"Is your number {midpoint}?\n1. Yes\n2. Too low\n3. Too high")
+        guess = int(input())
+        if guess == 1: # If the guess is correct
+            print(f"I got it in {guess_count + 1} tries!")
+            break
+        elif guess == 2: # If the guess is too low
+            guess_count += 1
+            low_range = midpoint
+            midpoint = low_range + int(((high_range - low_range) / 2))
+        else: # if guess is too high
+            guess_count += 1
+            high_range = midpoint
+            midpoint = low_range + int(((high_range - low_range) / 2))
+
+    print("Thanks for playing!")
+(guessing_game())
 
 
-
-
-
+# 
 
 
 
--- python_practice/beginner/basic/test_runer.py
+++ python_practice/beginner/basic/test_runer.py
@@ -1,18 +1,23 @@
-# fibonacci number generation:
+def guessing_game():
+    high_range = int(input("What is the number range?\n1 to ..."))
+    low_range = 1
+    midpoint = int(high_range / 2)
+    guess_count = 0
 
-def fibonacci():
-    num = int(input("Enter a number to check fibonacci....."))
-    i = 1
-    if num == 0:
-        fib = []
-    elif num == 1:
-        fib = [1]
-    elif num == 2:
-        fib = [1,1]
-    elif num > 2:
-        fib = [1,1]
-        while i < (num - 1):
-            fib.append(fib[i] + fib[i-1])
-            i += 1
-    return fib
-print(fibonacci())    
+    while True:
+        print(f"Is your number {midpoint}?\n1. Yes\n2. Too low\n3. Too high")
+        guess = int(input())
+        if guess == 1: # If the guess is correct
+            print(f"I got it in {guess_count + 1} tries!")
+            break
+        elif guess == 2: # If the guess is too low
+            guess_count += 1
+            low_range = midpoint
+            midpoint = low_range + int(((high_range - low_range) / 2))
+        else: # if guess is too high
+            guess_count += 1
+            high_range = midpoint
+            midpoint = low_range + int(((high_range - low_range) / 2))
+
+    print("Thanks for playing!")
+(guessing_game())
