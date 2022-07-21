d5fac501a63f4b537dcbf6ea5409cf97a75cb777
TimzOwen <timzowen@gmail.com>
2022-07-21 13:59:42 +0300

    string constance

--- README.md
+++ README.md
@@ -1,3 +1,37 @@
 ## python_practice
 
 python practice with questions and code sample solutions 
+
+### String constance 
+
+**string.ascii_letters**
+
+The concatenation of the ascii_lowercase and ascii_uppercase constants described below. This value is not locale-dependent.
+
+**string.ascii_lowercase**
+
+The lowercase letters /*'abcdefghijklmnopqrstuvwxyz'.*/ This value is not locale-dependent and will not change.
+
+**string.ascii_uppercase**
+
+The uppercase letters /*'ABCDEFGHIJKLMNOPQRSTUVWXYZ'*/. This value is not locale-dependent and will not change.
+
+**string.digits**
+
+The string '0123456789'.
+
+**string.hexdigits**
+
+The string /*'0123456789abcdefABCDEF'*/.
+
+**string.octdigits**
+
+The string '01234567'.
+
+**string.punctuation**
+
+String of ASCII characters which are considered punctuation characters in the C locale: /*!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~.*/
+
+**string.printable**
+
+String of ASCII characters which are considered printable. This is a combination of /*digits, ascii_letters, punctuation, and whitespace.*/
--- python_practice/beginner/basic/test_runer.py
+++ python_practice/beginner/basic/test_runer.py
@@ -1,29 +1,14 @@
-# write a function that returns a sentence with its characters in reverse oder
-# used 5 diffrrenr options to solve this question 
+# create a password generator using randome char 
+import string
+import random
 
-from curses import init_pair
-from ntpath import join
-
-
-def reverse_sentence(words): # using string and split 
-    split_word = words.split()
-    join_word = " ".join(split_word[::-1])
-    return join_word
-print(reverse_sentence("This is reversed"))
-
-def word_join(x):   #using one lineer killer 
-    return " ".join(x.split()[::-1])
-
-print(word_join("This is owen testing"))
-
-def word_reversedby(y): # using reversed keyword
-    w = y.split()
-    return " ".join(reversed(w))
-print(word_reversedby("using reversed key word"))
-
-def word_split_join(w):
-    w_split = w.split()
-    w_split.reverse()
-    return " ".join(w_split)
-print(word_split_join("This is reversed too"))
-
+def password_generator():
+    set_combine1 = string.ascii_letters
+    set_combine3 = string.digits
+    set_combine4 = string.punctuation
+    
+    all_combination = set_combine1  + set_combine3 + set_combine4
+    password = "".join(random.choices(all_combination,k=8))
+    
+    return password
+print(password_generator())
