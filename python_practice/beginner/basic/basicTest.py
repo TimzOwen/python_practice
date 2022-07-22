0308cf920b055123ce7eb2ffc434336887468833
TimzOwen <timzowen@gmail.com>
2022-07-22 21:46:10 +0300

    greatet among number

--- python_practice/beginner/basic/basicTest.py
+++ python_practice/beginner/basic/basicTest.py
@@ -889,3 +889,13 @@ also_no_winner = [[1, 2, 0],
 	[2, 1, 0]]
 
 print(checkGrid(also_no_winner))
+
+# function to choose between 3 greatest
+def greatest_number(a,b,c):
+    if a > b and a > c:
+        print("a is greatest")
+    elif b>a and b > c:
+        print("b is greatest")
+    else:
+        print("c is greatest")
+greatest_number(5,10,15)
